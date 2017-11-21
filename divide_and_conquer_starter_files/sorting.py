e690c056915ff9b44a1f68824ca22f5a8f4e4715
mrmthornton <mrmthornton@gmail.com>
2017-11-20 20:00:30 -0500

    three partition losing track of middle partition ends

--- divide_and_conquer_starter_files/sorting.py
+++ divide_and_conquer_starter_files/sorting.py
@@ -5,19 +5,21 @@ import time
 
 def partition3(a, L, r):
     x = a[L]
-    j = L; k = L
+    j = L; k = 0
     for i in range(L+1,r+1):
-        if a[i] <= x:
-            j += 1;k += 1
+        if a[i] < x:
+            j += 1
             a[i], a[j] = a[j], a[i]
         elif a[i] == x:
-            a[i], a[k] = a[k], a[i]
             k += 1
+            a[i], a[j+k] = a[j+k], a[i]
         else:
             #k += 1
             pass
     a[L], a[j] = a[j], a[L]
-    return (j,k)
+    print (" {}   {}   {}   {}".format(L,j,k,r))
+    print(a)
+    return (j,j+k)
             
 
 def partition2(a, l, r):
@@ -25,13 +27,16 @@ def partition2(a, l, r):
     j = l;
     for i in range(l + 1, r + 1):
         if a[i] <= x:
-            j += 1
             a[i], a[j] = a[j], a[i]
+            j += 1
     a[l], a[j] = a[j], a[l]
+    #print (" {}   {}   {}".format(l,j,r))
+    #print(a)
     return j
 
 
 def randomized_quick_sort3(a, l, r):
+    random.seed(17)
     if l >= r:
         return
     k = random.randint(l, r)
@@ -46,33 +51,48 @@ def randomized_quick_sort(a, l, r):
         return
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
-    #use partition2 
+    #use partition3
     m = partition2(a, l, r)
     randomized_quick_sort(a, l, m - 1);
     randomized_quick_sort(a, m + 1, r);
 
 def test_randomized_quick_sort3():
     random.seed(1)
-    a = [random.randint(0,10) for _ in range(1000)]
+    a = [random.randint(0,10) for _ in range(10000)]
     b = a.copy()
+    
     start=time.time()
     randomized_quick_sort3(a, 0, len(a)-1)
     finish=time.time()
     print("{:.10}".format(finish-start))
+    #print(a)
+    #print(b)
     start=time.time()
-    randomized_quick_sort3(b, 0, len(a)-1)
+    randomized_quick_sort(b, 0, len(a)-1)
     finish=time.time()
     print("{:.10}".format(finish-start))
+    assert(a==b)
 
+ 
 
 if __name__ == '__main__':
-    data_input = sys.stdin.read()
-    #data_input = input("n and n ints : ")
+    #data_input = sys.stdin.read()
+    data_input = input("n and n ints : ")
     n, *a = list(map(int, data_input.split()))
     if n == 0:
         test_randomized_quick_sort3()
     else:
-        randomized_quick_sort(a, 0, len(a)-1)
-        #randomized_quick_sort3(a, 0, len(a)-1)
+        b = a.copy()
+        #print(a)
+        #print(b)
+        
+        randomized_quick_sort3(a, 0, len(a)-1)
+        print("a = ", end =' ')
         for x in a:
+            print(x, end=' ')
+        
+        print()
+        randomized_quick_sort(b, 0, len(a)-1)
+        print("b = ", end =' ')
+        for x in b:
             print(x, end=' ')
