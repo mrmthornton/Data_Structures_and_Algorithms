fda465afc4ceed104224cc0f3a43085735e656ea
mrmthornton <mrmthornton@gmail.com>
2018-01-01 11:29:14 -0500

    closest.py is running about double the time allocated for some tests

--- greedy_algorithms_starter_files/largest_number.py
+++ greedy_algorithms_starter_files/largest_number.py
@@ -7,28 +7,39 @@ import random
 
 def test():
     random.seed(36963)
+    count = 0
     while True:
         bignum = random.randint(0,sys.maxsize)
-        bignum_str = str(bignum)
-        print(bignum_str)
+        bignum_str = str(bignum)+str(bignum)+str(bignum)+str(bignum)
+        #print(bignum_str)
         data = []
         L = len(bignum_str)
         start=0
         while L>0:
-            chunk_size = random.randint(1,L) 
-            data.append(bignum_str[start:start+chunk_size])
+            chunk_size = random.randint(1,L)
+            chunk = bignum_str[start:start+chunk_size]
+            while chunk[0] == '0' and len(chunk)>1:
+                chunk = chunk[1:]
+            data.append(chunk)
+            if len(data)>=8: # too slow after 9 elements
+                break
             start = start+chunk_size
             L = L-chunk_size
-        print("data = ",data)
+        #print("data = ",data)
         largest = largest_number(data)
+        #print("data = ",data)
         correct = nieve_largest_number(data)
         if largest == correct:
-            print("OK!")
+            if count == 100:
+                print("OK!",end='')
+                count = 0
+            count += 1
             continue
         else:
             print("ERROR:")
+            print("data = ",data)
             print("nieve: ",correct)
-            print("argor: ",largest)
+            print("smart: ",largest)
             break
             
 
@@ -37,15 +48,19 @@ def total_order(longest, str_num):
     multiplier = longest//L + 1
     str_num_fill = "".join([str_num]*multiplier)
     str_num_fill = str_num_fill[:longest]
-    #print(str_num_fill)
-    return(float("0." + str_num_fill))
+    #print(int(str_num_fill))
+    return(int(str_num_fill))
+    #return(float("0." + str_num_fill))
     
     
 def largest_number(a):
-    longest = len(sorted(a,key=len,reverse=True)[0])
-    total_order_longest = partial(total_order,longest)
+    longest = max(len(e) for e in a)
+    #print("longest = ", longest)
+    total_order_longest = partial(total_order,2*longest)
     b = sorted(a,key=total_order_longest ,reverse=True)
+    #print("b = ",b)
     res = "".join(b)
+    #print(res)
     return res
 
 
@@ -72,12 +87,14 @@ def nieve_largest_number(data):
     
 
 if __name__ == '__main__':
-    #data_input = sys.stdin.read()
-    data_input = input()
+    data_input = sys.stdin.read()
+    #data_input = input()
     data = data_input.split()
     if data[0]==str(0):
         test()
     else:
         a = data[1:]
+        #print(nieve_largest_number(a))
         print(largest_number(a))
+        
     
