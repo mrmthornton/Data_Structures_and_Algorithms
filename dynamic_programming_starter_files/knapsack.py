ef924e489f858407590f049eef348945d9702df8
mrmthornton <mrmthornton@gmail.com>
2018-01-02 14:13:00 -0500

    passed first two exercises

--- dynamic_programming_starter_files/knapsack.py
+++ dynamic_programming_starter_files/knapsack.py
@@ -2,6 +2,23 @@
 import sys
 
 def optimal_weight(W, w):
+    n = len(w)
+    w.insert(0,0)
+    v = w.copy()
+    value = [[0 for col in range(n+1)] for row in range(W+1)] 
+    
+    for i in range(1,n+1):
+        for weight in range(1,W+1):
+            value[weight][i] = value[weight][i-1]
+            if w[i] <= weight:
+                val = value[weight-w[i]][i-1] + v[i]
+                if value[weight][i] < val:
+                    value[weight][i] = val
+    return value[W][n]
+    
+    return value
+
+def less_than_optimal_weight(W, w):
     # write your code here
     result = 0
     for x in w:
@@ -10,6 +27,8 @@ def optimal_weight(W, w):
     return result
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    W, n, *w = list(map(int, input.split()))
+    data_input = sys.stdin.read()
+    #data_input = input("data in : ")
+    W, n, *w = list(map(int, data_input.split()))
+    #print(less_than_optimal_weight(W, w))
     print(optimal_weight(W, w))
--- dynamic_programming_starter_files/primitive_calculator.py
+++ dynamic_programming_starter_files/primitive_calculator.py
@@ -2,6 +2,40 @@
 import sys
 
 def optimal_sequence(n):
+    seq = [(0,0)] * (n+1)
+    
+    for i in range(1,n+1):
+        
+        count1 = (seq[i-1][0]+1, 1)
+        seq[i] = count1
+        
+        if i%2==0:
+            count2 = (seq[i//2][0]+1, 2)
+            if count2[0] < seq[i][0]:
+                seq[i] = count2
+            
+        if i%3==0:
+            count3 =  (seq[i//3][0]+1, 3)
+            if count3[0] < seq[i][0]:
+                seq[i] = count3 
+    
+    numbers = []
+    i = n
+    while i >= 1:
+        numbers.append(i)
+        op = seq[i][1]
+        if op == 1:
+            i = i - 1
+        if op == 2:
+            i = i//2
+        if op == 3:
+            i = i//3
+            
+    return list(reversed(numbers))
+
+    
+    
+def simple_sequence(n):
     sequence = []
     while n >= 1:
         sequence.append(n)
@@ -11,11 +45,20 @@ def optimal_sequence(n):
             n = n // 2
         else:
             n = n - 1
-    return reversed(sequence)
+    return list(reversed(sequence))
 
-input = sys.stdin.read()
-n = int(input)
-sequence = list(optimal_sequence(n))
-print(len(sequence) - 1)
-for x in sequence:
+data_input = sys.stdin.read()
+#data_input = input("data in : ")
+data = list(map(int, data_input.split()))
+n = data[0]
+
+#simple = simple_sequence(n)
+#print(len(simple) - 1)
+#for x in simple:
+#    print(x, end=' ')   
+#print()
+
+optimal = optimal_sequence(n)
+print(len(optimal) - 1)
+for x in optimal:
     print(x, end=' ')
